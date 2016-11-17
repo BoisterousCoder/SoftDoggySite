bf79fd868ea4511937afac01fa186ff6682c208a
Chris <Shadowace248@gmail.com>
2016-11-17 12:06:53 -0700

    pug data fix

--- compile.js
+++ compile.js
@@ -1,11 +1,11 @@
 const pug = require('pug');
 const fs = require('fs');
 
-let files = fs.readdirSync('./Jade');
+let files = fs.readdirSync('./PUG');
 
 files.forEach(function (fileName) {
     let fileExtention = '.' + fileName.split('.')[1];
     fileName = fileName.split('.')[0];
-    let htmlText = pug.compileFile('./Jade/' + fileName + fileExtention)({});
+    let htmlText = pug.compileFile('./PUG/' + fileName + fileExtention)({});
     fs.writeFile('./' + fileName + '.html', htmlText);
 });
---
+++ compileJade.js
@@ -0,0 +1,5 @@
+var fs = require('fs');
+var jade = require('jade');
+ 
+// compile 
+var fn = jade.compile('string of jade', options);
